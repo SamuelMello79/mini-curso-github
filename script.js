493cc866f8f1f412a85fe42aadd626861df807c9
Samuel Mello <mello.developer797@gmail.com>
2024-09-01 15:40:16 -0300

    Atualizações na Interface

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function entrarUsuario() {
-    console.log("Usario entrou!");
+function entrarNoCurso() {
+    console.log("Entrou no curso!");
+}
+
+function sairDoCurso() {
+    console.log("Saiu do curso!")
 }
